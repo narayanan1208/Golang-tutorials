773dd900d0d2f41ac764a6c7ba359735ea7e6984
narayanan1208 <narayanan6647@gmail.com>
2025-02-07 12:03:26 +0530

    Golang defer example

---
+++ 17defer/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+// defer follows last-in first out. when defer is used before a line, it gets executed last.
+func main() {
+	defer fmt.Println("World")
+	defer fmt.Println("One")
+	defer fmt.Println("Two")
+	fmt.Println("Hello")
+	myDefer()
+}
+
+func myDefer() {
+	for i := 0; i < 5; i++ {
+		defer fmt.Print(i)
+	}
+}
