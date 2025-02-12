489260d2877e191083db7ea7ffa6f2df597fafba
narayanan1208 <narayanan6647@gmail.com>
2025-02-12 13:56:32 +0530

    Golang hello world example

---
+++ Hello_world/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello")
+}
+
+// go run filename
+// go env GOPATH
+// go is case insensitive
+// types - String, Bool, Integer, Floating, Complex, Array, Slices, Maps, Structs(instead of Class), Pointers
